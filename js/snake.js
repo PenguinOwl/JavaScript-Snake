97922c651083ede69e5e24a24bea06842c61f559
Henry <henryjboles@gmail.com>
2017-09-20 11:34:01 -0700

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -121,15 +121,15 @@ SNAKE.Snake = SNAKE.Snake || (function() {
             rowShift = [-1, 0, 1, 0],
             xPosShift = [],
             yPosShift = [],
-            snakeSpeed = 75,
+            snakeSpeed = 50,
             isDead = false,
             isPaused = false;
         function getMode (mode, speed) {
     document.getElementById(mode).addEventListener('click', function () { snakeSpeed = speed; });
 }
-            getMode('Easy', 100);
-            getMode('Medium', 75);
-            getMode('Difficult', 50);
+            getMode('Easy', 75);
+            getMode('Medium', 50);
+            getMode('Difficult', 30);
         // ----- public variables -----
         me.snakeBody = {};
         me.snakeBody["b0"] = new SnakeBlock(); // create snake head
